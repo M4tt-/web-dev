9c0de5b5f69e7534720187c1c2a3c79563b7836b
M4tt- <mtrunyon91@gmail.com>
2018-07-10 17:37:01 -0400

    Added 'pic' column to posts table

--- sql/createBlogDB.sql
+++ sql/createBlogDB.sql
@@ -15,7 +15,8 @@ CREATE TABLE posts (
 			 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 			 title VARCHAR(255) NOT NULL,
 			 submissiondate DATETIME NOT NULL DEFAULT NOW(),
-			 post TEXT NOT NULL
+			 post TEXT NOT NULL,
+			 pic VARCHAR(255)
 			 );
 			 
 CREATE TABLE comments (
